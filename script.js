75b7ccceacd571ae75f4e38324d9f95c5e77eab0
Raj Chaurasiya <rc808129@gmail.com>
2025-10-03 11:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+// Is code ko ek alag JS file (jaise script.js) mein rakhein
+
+// Pehle element ko select karein
+const element = document.getElementById("level");
+
+// DOM level calculate karne ke liye function
+function getDomLevel(el) {
+    let level = 0;
+    let current = el;
+
+    // Jab tak parent element milta rahe, loop chalao
+    while (current.parentNode && current.tagName !== 'HTML') {
+        current = current.parentNode;
+        level++;
+    }
+    return level;
+}
+
+// Element ka DOM level calculate karein
+const domLevel = getDomLevel(element);
+
+// alert() function ka use karke result display karein
+alert("The DOM level of the element is: " + domLevel);
